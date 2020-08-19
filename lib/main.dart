d202191c42022c421816b044a277addb8e22fd0a
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-19 18:57:14 +0900

    Imported runApp and combine with print function.

--- lib/main.dart
+++ lib/main.dart
@@ -5,5 +5,12 @@ import "package:flutter/material.dart";
 
 
 void main(){
-
+  /*
+  *  Print text methods.
+  *  1. Using runApp(Text(""));
+  *  2. Using print(""); only.
+  * */
+  runApp(Text("Hello Flutter",
+      textDirection: TextDirection.ltr, ));
+  print("Hello World");
 }
